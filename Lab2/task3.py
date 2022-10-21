306b18733bd664b96e468dc3b368bb4508b66fb2
Dimaneug <dimaneug@gmail.com>
2022-10-21 21:14:40 +0300

    Добавлен текстовый файл 4.txt.\nДоделано до конца задание 3 в task3.py

--- Lab2/task3.py
+++ Lab2/task3.py
@@ -1,6 +1,6 @@
 import math
 import os
-
+from chardet.universaldetector import UniversalDetector
 
 def task3():
     count = [0] * 256
@@ -37,10 +37,36 @@ def task3():
     for symbol in table_without_ascii[0:4]:
         print("%5x %7d %11.2f %17.2f" % tuple(symbol))
     print("-------------------------------------------------------\n")
+  
+
+def encoding_specifier(filename):
+    count = [0] * 256
+    encodings = ["windows-1251", "KOI8-R", "IBM866", "ISO-8859-5"]
+    for encoding in encodings:
+        with open(filename, 'rb') as f:
+            byte = f.read(1)
+            while byte != b'':
+                count[int.from_bytes(byte, byteorder="big")] += 1
+                byte = f.read(1)
+        table = []
+        for i in range(256):
+            if count[i] != 0:
+                table.append([(i.to_bytes((i.bit_length() + 7) // 8, 'big'))\
+                    .decode(encoding=encoding), count[i]])
+        table.sort(key=lambda x: x[1], reverse=True)
+        print("-------------------------------------------------------")
+        print(encoding)
+        print("-------------------------------------------------------")
+        print("Символ Кол-во\u2193")
+        for symbol in table:
+            print("%6r %7d" % tuple(symbol))
+        print("-------------------------------------------------------\n")
+    
 
 
 def main():
-    task3()
+    #task3()
+    encoding_specifier("4.txt")
 
 
 if __name__ == "__main__":
